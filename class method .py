4660ac80948c385e2cba6e55d187c1f0ff176792
Shadabashraf1926 <shadab2882@gmail.com>
2023-12-18 17:03:54 +0530

    my files

--- class method .py
+++ class method .py
@@ -1,40 +1,40 @@
-# '''A class method is a method which is bound to the class only
-# and not the instances of the class.
+'''A class method is a method which is bound to the class only
+and not the instances of the class.
 
-# Its take class itself as it first argument  conventionallly names as "cls" 
+Its take class itself as it first argument  conventionallly names as "cls" 
 
-# Class method are defined using "@classmethod" decoraters in python
+Class method are defined using "@classmethod" decoraters in python
 
-# '''
+'''
 
 
-# ## baisc syntaxx
+## baisc syntaxx
 
-# class My_class:
+class My_class:
     
-#     @classmethod
-#     def my_class_method(cls,name,gmail,roll_no):
-#         print(name,gmail,roll_no) 
+    @classmethod
+    def my_class_method(cls,name,gmail,roll_no):
+        print(name,gmail,roll_no) 
     
     
-# My_class.my_class_method("shadab", "shadab@gmail.com", 234)
+My_class.my_class_method("shadab", "shadab@gmail.com", 234)
     
-# '''Here, we dont have to actually make a object for the class.
-#   we can directly call our method'''    
+'''Here, we dont have to actually make a object for the class.
+  we can directly call our method'''    
   
  
-# # let us take another example
+# let us take another example
 
 
 
-# class school:
+class school:
        
-#         @classmethod
-#         def my_details(cls,name,address,rank):
-#             print(name,address,rank)
+        @classmethod
+        def my_details(cls,name,address,rank):
+            print(name,address,rank)
 
 
-# obj = school.my_details("Indian Public School" , "Rajabazar" , 1)
+obj = school.my_details("Indian Public School" , "Rajabazar" , 1)
 
 
 
